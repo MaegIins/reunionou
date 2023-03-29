013a75b9114d0801962f24c9a9c8c67e8e82e927
Tristan <62724143+MaegIins@users.noreply.github.com>
2023-03-29 16:27:25 +0200

    debut pages login et signup

---
+++ src/components/LogInComp.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1>LogIn</h1>
+    <input v-model="name" placeholder="USERNAME"/>
+    <input v-model="password" placeholder="PASSWORD"/>
+    <button v-on:click="logIn">LOG IN</button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+
+
+  name: "log",
+  data() {
+    return {
+      name: "",
+      password: "",
+    };
+  },
+
+  methods: {
+    logIn() {
+      axios
+          .post("http://localhost:33323/auth/signin", {}, {
+            auth:{
+              username: this.name,
+              password: this.password,
+            }
+          })
+          .then((response) => {
+            console.log(response);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/SignUpComp.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1>SignIn</h1>
+    <input v-model="name" placeholder="USERNAME"/>
+    <input v-model="email">
+    <input v-model="password" placeholder="PASSWORD"/>
+    <button v-on:click="signIn">SIGN IN</button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "SignUpComp",
+  data() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    signIn() {
+      axios
+        .post("http://localhost:33323/auth/signup", {
+          name: this.name,
+          email: this.email,
+          password: this.password,
+        })
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
