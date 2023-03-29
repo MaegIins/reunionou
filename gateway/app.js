3fc3a59e50b07cd516dccb67103bea4da8b0d72c
Dal Bon Quentin <dalbon3u@etu.univ-lorraine.fr>
2023-03-29 15:38:22 +0200

    add cors to gateway (normalement je test)

--- gateway/app.js
+++ gateway/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const logger = require('morgan');
 const helmet = require("helmet");
+var cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const authRouter = require('./routes/auth');
@@ -9,6 +10,7 @@ const invitesRouter = require('./routes/invites');
 
 
 const app = express();
+app.use(cors());
 
 const env = process.env.NODE_ENV;
 
