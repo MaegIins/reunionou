4e3a719a60fe5925409b073d30fff2b5fca07bc4
lgyn57 <axel.leguyen@gmail.com>
2023-03-30 12:25:12 +0200

    acces details events by invites's  key (gateway)

--- gateway/routes/invites.js
+++ gateway/routes/invites.js
@@ -19,6 +19,23 @@ router.post('/confirm', async (req, res, next) => {
     }
   });
 
+  // get details of an event
+router.get('/', async (req, res, next) => {
+    try {
+      const { key } = req.query;
+          await axios.get('http://events:3000/invites/details', { params: { key } })
+            .then((response) => {
+              res.json(response.data);
+            })
+            .catch((error) => {
+              res.status(404).json(error.response.data);
+            });
+    }
+    catch (error) {
+      res.status(500).json(error);
+    }
+  });
+  
 
 
 module.exports = router;
