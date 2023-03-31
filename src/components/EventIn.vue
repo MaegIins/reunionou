389eeb47c627796d9e33959c70e35e710c342699
lgyn57 <axel.leguyen@gmail.com>
2023-03-31 11:58:59 +0200

    comments

--- src/components/EventIn.vue
+++ src/components/EventIn.vue
@@ -44,12 +44,11 @@ export default {
     attendee() {
 
       const config = {
-        headers: { Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywibmFtZSI6IlF1ZW50aW4iLCJtYWlsIjoicXVlbnRpbkBpdXQuZnIiLCJpYXQiOjE2ODAxODgxOTgsImV4cCI6MTY4MDE5MTc5OH0.dfnlFKQWBB30OP4v85RZ9ZtFkeqTZzxATP9SPArPmDg` }
+        headers: { Authorization: `Bearer ${sessionStorage.getItem("access_token")}` }
       };
       axios.get("http://localhost:3333/events/" + this.idEvent + "/attendees", config)
         .then((response) => {
           this.people = response.data
-          console.log(response.data)
         })
         .catch((error) => {
           console.log(error)
@@ -57,25 +56,26 @@ export default {
 
     },
 
-    //faire la même chose que les participant pour les com, mettre les comments dans la gateway
-    // comments() {
+    // faire la même chose que les participant pour les com, mettre les comments dans la gateway
+    com(){
 
-    //   const config = {
-    //     headers: { Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywibmFtZSI6IlF1ZW50aW4iLCJtYWlsIjoicXVlbnRpbkBpdXQuZnIiLCJpYXQiOjE2ODAxODgxOTgsImV4cCI6MTY4MDE5MTc5OH0.dfnlFKQWBB30OP4v85RZ9ZtFkeqTZzxATP9SPArPmDg` }
-    //   };
-    //   axios.get("http://localhost:3333/events/" + this.idEvent + "/attendees", config)
-    //     .then((response) => {
-    //       this.people = response.data
-    //       console.log(response.data)
-    //     })
-    //     .catch((error) => {
-    //       console.log(error)
-    //     });
+      const config = {
+        headers: { Authorization: `Bearer ${sessionStorage.getItem("access_token")}` }
+      };
+      axios.get("http://localhost:3333/comments/events/" + this.idEvent, config)
+        .then((response) => {
+          this.comments = response.data
+          console.log(response.data)
+        })
+        .catch((error) => {
+          console.log(error)
+        });
 
-    // },
+    },
   },
   mounted() {
-    this.attendee()
+    this.attendee(),
+    this.com()
   },
 
   props: {},
@@ -85,43 +85,7 @@ export default {
       idEvent: this.$route.params.id,
       eventPosition: [48.8566, 2.3522],
       people: [],
-      comments: [{
-        id: 1, name: "Jean", comment: "Je suis là !",
-        date: "13:23 12/12/2020"
-      },
-      {
-        id: 2, name: "Pierre", comment: "Je ne serai pas là !",
-        date: "13:23 12/12/2020"
-      },
-      {
-        id: 3, name: "Paul", comment: "Je ne sais pas encore !",
-        date: "13:23 12/12/2020"
-      },
-      {
-        id: 4, name: "Jacques", comment: "Je suis là !",
-        date: "13:23 12/12/2020"
-      },
-      {
-        id: 5, name: "Marie", comment: "Je ne serai pas là !",
-        date: "13:23 12/12/2020"
-      },
-      {
-        id: 6, name: "Jeanne", comment: "Je ne sais pas encore !",
-        date: "13:23 12/12/2020"
-      },
-      {
-        id: 7, name: "Pierre", comment: "Je suis là !",
-        date: "13:23 12/12/2020"
-      },
-      {
-        id: 8, name: "Paul", comment: "Je ne serai pas là !",
-        date: "13:23 12/12/2020"
-      },
-      {
-        id: 9, name: "Jacques", comment: "Je ne sais pas encore !",
-        date: "13:23 12/12/2020"
-      }
-      ],
+      comments: [],
 
     };
   }
