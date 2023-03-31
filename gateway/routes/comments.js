3a8945566dd0161484505934e68337035e8129ef
lgyn57 <axel.leguyen@gmail.com>
2023-03-31 11:21:01 +0200

    comments get

--- gateway/routes/comments.js
+++ gateway/routes/comments.js
@@ -8,7 +8,7 @@ router.get('/events/:id', async (req, res, next) => {
     try {
         await axios.get('http://auth:3000/validate', { headers: { 'Authorization': req.headers.authorization } })
             .then(async (validateResponse) => {
-                await axios.get('http://events:3000/comments/events'+ req.params.id)
+                await axios.get('http://events:3000/comments/events/'+ req.params.id)
                     .then((response) => {
                         res.status(response.status).json(response.data);
                     })
@@ -23,6 +23,10 @@ router.get('/events/:id', async (req, res, next) => {
     catch (error) {
         res.status(500).json(error);
     }
+});
+
+router.get('/', async (req, res, next) => {
+    res.status(200).json({ message: 'Hello' });
 });
 
 
