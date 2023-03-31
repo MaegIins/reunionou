9fad7a2605e08ccdf48d9818dab7d37e9bfa4d15
Farronilucas <farronilucas@hotmail.fr>
2023-03-31 15:33:34 +0200

    add ok

--- lib/providers/events_provider.dart
+++ lib/providers/events_provider.dart
@@ -62,7 +62,7 @@ Future<void> addEvent(String bearerToken, EventAdress eventAdress) async {
     }),
   );
 
-  if (response.statusCode != 201) {
+  if (response.statusCode != 201 && response.statusCode != 302 && response.statusCode != 200) {
     throw Exception('Failed to add event');
   }
 }
