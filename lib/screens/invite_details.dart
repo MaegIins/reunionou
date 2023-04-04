de7bbbd9f7eb5af8d2ff57f9e67d7c3ca05c2238
Farronilucas <farronilucas@hotmail.fr>
2023-04-04 13:48:16 +0200

    accept invitation

--- lib/screens/invite_details.dart
+++ lib/screens/invite_details.dart
@@ -1,22 +1,99 @@
 import 'package:flutter/material.dart';
 import '../models/Invite.dart';
 import '../Singleton/Auth.dart';
-import 'invitation_list.dart';
 import 'package:partouille/providers/invites_provider.dart';
+import '../models/inviteConfirm.dart';
+import 'invitation_list.dart';
 
-class InviteDetailsPage extends StatelessWidget {
-  final  Invite inviteDetails;
+class InviteDetailsPage extends StatefulWidget {
+  final Invite inviteDetails;
 
   const InviteDetailsPage({Key? key, required this.inviteDetails}) : super(key: key);
+
+  @override
+  _InviteDetailsPageState createState() => _InviteDetailsPageState();
+}
+
+class _InviteDetailsPageState extends State<InviteDetailsPage> {
+  bool? _selectedOption;
+  String? _comment;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(inviteDetails.name ?? ''),
+        title: Text(widget.inviteDetails.name ?? ''),
       ),
       body: Center(
-        child: Text('Détails de l\'invitation'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Détails de l\'invitation'),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      _selectedOption = true;
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.green,
+                    padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                  ),
+                  child: Text('Je viens'),
+                ),
+                SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      _selectedOption = false;
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.red,
+                    padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                  ),
+                  child: Text('Je ne viens pas'),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            TextField(
+              onChanged: (value) {
+                setState(() {
+                  _comment = value;
+                });
+              },
+              decoration: InputDecoration(
+                hintText: 'Ajouter un commentaire (optionnel)',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                if (_selectedOption != null) {
+                   final bearerToken = "Bearer " + Auth().token;
+                  final InviteConfirm = inviteConfirm(
+                    idEvent: widget.inviteDetails.id_event,
+                    status: _selectedOption!,
+                    comment: _comment ?? '',
+                  );
+                  InvitesProvider().reponseInvitation(bearerToken, InviteConfirm);
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(
+                      builder: (context) => InvitationListPage(),
+                    ),
+                  );
+                }
+              },
+              child: Text('Envoyer la réponse'),
+            ),
+          ],
+        ),
       ),
     );
   }
